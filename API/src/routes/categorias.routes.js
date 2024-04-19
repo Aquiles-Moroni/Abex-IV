6bec595e80e2189567ef99709c32a248c23ff0a7
Jeferson Rocha <111543601+jeferocha1997@users.noreply.github.com>
2024-04-18 21:48:55 -0300

    upload Backend 18/04 ak- files

---
+++ API/src/routes/categorias.routes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import * as userController from '../controllers/userController.js';
+
+const router = express.Router();
+
+router.get('/users', userController.getUsers);
+router.post('/users', userController.createUser);
+router.get('/users/:id', userController.getUserById);
+router.put('/users/:id', userController.updateUserById);
+router.delete('/users/:id', userController.deleteUserById);
+
+export default router;
